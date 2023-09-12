e9ccacaa93711ac1bc7323b687cb4b0a1bacd217
iamraufu <eftykharraufu@gmail.com>
2023-09-12 23:25:53 +0600

    [UPDATED] Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import logo from '../images/logo.svg'
+import sidebarData from '../data/sidebar/sidebar';
 
 const Sidebar = () => {
-      return (
-            <div>
-                  <div className="d-flex justify-content-center align-items-center pt-5">
-                        <img src={logo} alt="Brand Logo" />
-                        <h1 className='title-text'><span className='title-text-primary'>Blue</span><span className='title-text-secondary'>Trade.</span></h1>
-                  </div>
+
+    return (
+        <div>
+            <div className="d-flex justify-content-center align-items-center pt-5">
+                <img src={logo} alt="Brand Logo" />
+                <h1 className='title-text'><span className='title-text-primary'>Blue</span><span className='title-text-secondary'>Trade.</span></h1>
             </div>
-      );
+            <div className="mt-5"></div>
+            {
+                sidebarData.map((item, index) =>
+                    index === sidebarData.length - 2 ?
+                        <div style={{ borderTop: "1px solid lightgrey" }} 
+                        key={index} className="d-flex justify-content-center align-items-center mt-5 mx-5 pt-5">
+                            <img src={item.image} alt={item.name} />
+                            <h2 className='sidebar-item mt-2 ms-2'>{item.name}</h2>
+                        </div>
+                        :
+                        <div key={index} className="d-flex justify-content-center align-items-center mt-4">
+                            <img src={item.image} alt={item.name} />
+                            <h2 className='sidebar-item mt-2 ms-2'>{item.name}</h2>
+                        </div>
+                )
+            }
+        </div>
+    );
 };
 
 export default Sidebar;
---
+++ src/data/sidebar/sidebar.js
@@ -0,0 +1,40 @@
+import dashboardIcon from '../../images/sidebar/dashboard.svg'
+import marketIcon from '../../images/sidebar/markets.svg'
+import transactionIcon from '../../images/sidebar/transactions.svg'
+import profileIcon from '../../images/sidebar/profile.svg'
+import settingIcon from '../../images/sidebar/setting.svg'
+import helpIcon from '../../images/sidebar/help.svg'
+import logoutIcon from '../../images/sidebar/logout.svg'
+
+const sidebarData = [
+    {
+        image: dashboardIcon,
+        name: "Dashboard"
+    },
+    {
+        image: marketIcon,
+        name: "Markets"
+    },
+    {
+        image: transactionIcon,
+        name: "Transactions"
+    },
+    {
+        image: profileIcon,
+        name: "Profile"
+    },
+    {
+        image: settingIcon,
+        name: "Setting"
+    },
+    {
+        image: helpIcon,
+        name: "Help"
+    },
+    {
+        image: logoutIcon,
+        name: "Log Out"
+    }
+]
+
+export default sidebarData;
